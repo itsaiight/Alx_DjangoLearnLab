664629e08de8e2d89177c1b6131806742367ebb1
Keamogetse <156374079+itsaiight@users.noreply.github.com>
2025-07-18 16:47:19 +0200

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 def books_by_author(author_name):
     author = Author.objects.get(name = author_name)
     books = Book.objects.filter(author = author)
-    print(f"Books by {author_name}:")
+    print(f"Books by {author.name}:")
     for book in books:
         print(f"- {book.title}")
 
