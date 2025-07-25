26eb4ecfdc6fbc8d151bde9caa4733707edfb47a
itsaiight <mahlaregosiame@gmail.com>
2025-07-25 16:04:25 +0200

    updated models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField(null=True, blank=True)
+
+    
+    class Meta:
+        permissions = [
+            ("can_create", "Can create a book")
+            ("can_view", "Can add a book")
+            ("can_edit", "Can change a book")
+            ("can_delete", "Can delete a book")
+        ]
 
 
 class CustomUserManager(BaseUserManager):
@@ -54,6 +66,25 @@ class CustomUser(AbstractUser):
         verbose_name = 'Custom User'
         verbose_name_plural = 'Custom Users'
         permissions = [
-            ('can_view_profile', 'Can view profile'),
-            ('can_edit_profile', 'Can edit profile'),
+            ('can_view_profile', 'Can view profile')
+            ('can_edit_profile', 'Can edit profile')
         ]
+
+
+book_ct = ContentType.objects.get_for_model(Book)
+
+# Create Groups
+editors, _ = Group.objects.get_or_create(name="Editors")
+viewers, _ = Group.objects.get_or_create(name="Viewers")
+admins, _ = Group.objects.get_or_create(name="Admins")
+
+# Assign Permissions
+can_view = Permission.objects.get(codename='can_view', content_type=book_ct)
+can_create = Permission.objects.get(codename='can_create', content_type=book_ct)
+can_edit = Permission.objects.get(codename='can_edit', content_type=book_ct)
+can_delete = Permission.objects.get(codename='can_delete', content_type=book_ct)
+
+# Assign to groups
+editors.permissions.set([can_view, can_create, can_edit])
+viewers.permissions.set([can_view])
+admins.permissions.set([can_view, can_create, can_edit, can_delete])
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import Group, Permission
-from django.contrib.contenttypes.models import ContentType
+
 
 
 # Create your models here.
@@ -18,11 +17,9 @@ class Book(models.Model):
     
     class Meta:
         permissions = [
-            ("can_add", "Can add a book"),
-            ("can_view", "Can view a book"),
-            ("can_create", "Can create a book"),
-            ("can_edit", "Can edit a book"),
-            ("can_delete", "Can delete a book"),
+            ("can_add", "Can add a book")
+            ("can_change", "Can change a book")
+            ("can_delete", "Can delete a book")
         ]
 
     def __str__(self):
@@ -52,24 +49,6 @@ class UserProfile(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
 
-
-
-
-# Get content type for Book
-book_ct = ContentType.objects.get_for_model(Book)
-
-# Create Groups
-editors, _ = Group.objects.get_or_create(name="Editors")
-viewers, _ = Group.objects.get_or_create(name="Viewers")
-admins, _ = Group.objects.get_or_create(name="Admins")
-
-# Assign Permissions
-can_view = Permission.objects.get(codename='can_view', content_type=book_ct)
-can_create = Permission.objects.get(codename='can_create', content_type=book_ct)
-can_edit = Permission.objects.get(codename='can_edit', content_type=book_ct)
-can_delete = Permission.objects.get(codename='can_delete', content_type=book_ct)
-
-# Assign to groups
-editors.permissions.set([can_view, can_create, can_edit])
-viewers.permissions.set([can_view])
-admins.permissions.set([can_view, can_create, can_edit, can_delete])
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from relationship_app.models import Book
