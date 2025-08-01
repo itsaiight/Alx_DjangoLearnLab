555d72b73693a6a6cebb71649363fd878ac6b52e
itsaiight <mahlaregosiame@gmail.com>
2025-08-01 13:35:41 +0200

    updated views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -8,4 +8,16 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        return Book.objects.all()
+    def perform_create(self, serializer):
+        serializer.save()  # Save the book instance
+    def perform_update(self, serializer):
+        serializer.save()
+    def perform_destroy(self, instance):
+        return super().perform_destroy(instance)
     
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -18,9 +18,17 @@ from django.contrib import admin
 from django.urls import path
 from api.views import BookList
 from django.urls import include
+from rest_framework.routers import DefaultRouter
+from api.views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('api/', include('api.urls')),  # Include API URLs
     path('admin/', admin.site.urls),
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),  # This includes all routes registered with the router
+    
 ]
