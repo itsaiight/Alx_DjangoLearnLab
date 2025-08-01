82d0bcb432bebfdc6f1492ba3c3bc22c46091716
itsaiight <mahlaregosiame@gmail.com>
2025-08-01 16:52:36 +0200

    updated urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path
 from api.views import BookList
 from django.urls import include
@@ -9,8 +8,7 @@ router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    path('api/', include('api.urls')),  # Include API URLs
-    path('admin/', admin.site.urls),
+    
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
     path('', include(router.urls)),  # This includes all routes registered with the router
 ]
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from api.views import BookList
 from django.urls import include
 from rest_framework.routers import DefaultRouter
 from api.views import BookViewSet
@@ -25,9 +24,7 @@ router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    path('api/', include('api.urls')),  # Include API URLs
     path('admin/', admin.site.urls),
-    path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
-    path('', include(router.urls)),  # This includes all routes registered with the router
+    path('api/', include('api.urls')),  # Include API URLs
     
 ]
