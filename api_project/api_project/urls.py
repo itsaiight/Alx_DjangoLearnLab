7614178b24db642a50b5e76b63c3396839b7365a
itsaiight <mahlaregosiame@gmail.com>
2025-08-01 13:21:55 +0200

    updated urls.py

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from api.views import BookList
+from django.urls import include
 
 urlpatterns = [
+    path('api/', include('api.urls')),  # Include API URLs
     path('admin/', admin.site.urls),
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
 ]
