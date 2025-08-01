eca306348e8b6121bf1b27bd94b4b1d71d331e34
itsaiight <mahlaregosiame@gmail.com>
2025-08-01 17:40:10 +0200

    updated settings.py and views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
 
 # Create your views here.
 class BookList(generics.ListAPIView):
@@ -11,7 +14,9 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]  # Only admin users can perform create, update, delete actions
+        
     def get_queryset(self):
         return Book.objects.all()
     def perform_create(self, serializer):
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from django.urls import include
 from rest_framework.routers import DefaultRouter
 from api.views import BookViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -26,5 +27,6 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Include API URLs
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     
 ]
