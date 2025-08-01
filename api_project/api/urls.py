ff5fc957bcbcd913b86fa2a56d2a3d0e53206266
itsaiight <mahlaregosiame@gmail.com>
2025-08-01 13:38:51 +0200

    updated api/urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from api.views import BookList
 from django.urls import include
+from rest_framework.routers import DefaultRouter
+from api.views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('api/', include('api.urls')),  # Include API URLs
