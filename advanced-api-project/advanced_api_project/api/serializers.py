53c92f1ea8c3e6e501e652bfac258d2b3ea9a10c
itsaiight <mahlaregosiame@gmail.com>
2025-08-04 01:25:16 +0200

    created advanced_api_project

---
+++ advanced-api-project/advanced_api_project/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Book, Author
+from datetime import date
+
+# Serializers for the Book model
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['title', 'publication_year', 'author']
+
+    def validate_publication_year(self, value):
+        # Validate that the publication year is not in the future
+        if value('publication_year') > date.today().year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
+
+class AuthorSerializer(serializers.ModelSerializer):
+    # Nested BookSerializer to include books by the author
+    book = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ['name', 'book'] 
---
+++ advanced-api-project/advanced_api_project/api/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+# Create your views here.
