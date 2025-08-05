be59a877e604ea7cf84bf08631a964d7c2811e75
itsaiight <mahlaregosiame@gmail.com>
2025-08-05 17:43:39 +0200

    updated advanced_api_project/urls.py

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from api.views import ListView, DetailView, CreateView, UpdateView, DeleteView
+from api.urls import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
