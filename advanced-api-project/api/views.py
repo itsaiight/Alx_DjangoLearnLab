0022751b620e0c2e34f0e3a0ff31f93a99537309
itsaiight <mahlaregosiame@gmail.com>
2025-08-05 17:41:39 +0200

    updated views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
 # ListView for listing all books
