525cb8b790d211d533c18c4f83b99e9998950140
itsaiight <mahlaregosiame@gmail.com>
2025-08-17 21:23:25 +0200

    updated post urls from 'posts' to 'post'

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -25,10 +25,10 @@ urlpatterns = [
     path('register/', SignUpView.as_view(template_name='blog/register.html'), name='register'),
     path('profile/', ProfileView.as_view(template_name='blog/profile.html'), name='profile'),
     path('logout/', views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('posts/', PostListView.as_view(), name='post-list'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/', PostListView.as_view(), name='post-list'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
 
