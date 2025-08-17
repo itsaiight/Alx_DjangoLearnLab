9607f2689cf43ac3a524e24bce86517d6d98aa6e
itsaiight <mahlaregosiame@gmail.com>
2025-08-17 22:48:29 +0200

    removed tag model and installed taggit, and updated post model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,21 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
-# Create your models here.
-class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=50, unique=True)
-    
-    def __str__(self):
-        return self.name
-    
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+    tags = TaggableManager(blank=True)
     
     def get_tags_display(self):
         return ", ".join(tag.name for tag in self.tags.all())
