c446b811d1bee09aa8d1ea305182370f5274e9ce
itsaiight <mahlaregosiame@gmail.com>
2025-08-21 21:25:47 +0200

    Created serializers for both models and viewsets for each

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
-
+from rest_framework import generics, viewsets
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 # Create your views here.
+
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        return Post.objects.all()
+    def perform_create(self, serializer):
+        serializer.save()  
+    def perform_update(self, serializer):
+        serializer.save()
+    def perform_destroy(self, instance):
+        return super().perform_destroy(instance)
+    
+class CommentList(generics.ListAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        return Comment.objects.all()
+    def perform_create(self, serializer):
+        serializer.save()  # Save the book instance
+    def perform_update(self, serializer):
+        serializer.save()
+    def perform_destroy(self, instance):
+        return super().perform_destroy(instance)
